66346c24427e439520ebc21d34b4b1af9e7b32fb
AlbinBerglund <berglund.albinx@gmail.com>
2021-12-21 15:41:04 +0200

    update redirct to a register page and other things

--- src/App.js
+++ src/App.js
@@ -1,139 +1,43 @@
-
-import { useRef, useState } from "react"
-import { login , useAuth } from "./firebase";
-import "./App.css"
-
+import Login from "./Login";
+import Register from "./Register";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
-export default function App() {
-const [ loading, setloading ] = useState(false);
-const currentUser = useAuth();
-
-  const emailRef = useRef();
-  const passwordRef = useRef();
-const WinH = window.screen.height;
-const WinW =  window.screen.width; 
-
-console.log(WinW, WinH)
+ function App() {
 
   
-if (WinW >= 320 && WinW <= 575) {
+
    return (
-    
-    <div className="App">
-      <div className="bg2">
-      <div className="container-fluid">
-        <div className="row">
-
-
-        <div className="col-md-4 col-sm-4 col-xs-12"></div>
-        <div className="col-md-4 col-sm-4 col-xs-12">
+      <div className="App">
         
-          
-        <form className="form-container2">
-          <img src="logo.png" alt=""></img>
+        <BrowserRouter>
 
-          <div className="mb-3">
-        <input type="text" placeholder="Email" ref={emailRef} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-        </div>
-        <div className="mb-3">
-        <input type="password" placeholder="Password" ref={passwordRef} className="form-control" id="exampleInputPassword1"/>
-        </div>
-        <div className="mb-3 form-check"></div>
-        <div className="d-grid"> 
-        <button type="submit" className="btn btn-success btn-primary" disabled={loading || currentUser } onClick={handleLogin}>Log In</button>
-        </div>
+        <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/Register' element={<Register/>}/>
+        </Routes>
+
+        
+        
+        
+        </BrowserRouter>
 
        
-        </form>
-
-          
-
-
-          
-          
-        </div>
-        <div className="col-md-4 col-sm-4 col-xs-12"></div>
-
-          
-
-
-        </div>
       </div>
-      </div>
-    </div>
     
   );
 }
-else{
-  return (
-    
-    <div className="App">
-      <div className="bg">
-      <div className="container-fluid">
-        <div className="row">
-
-
-        <div className="col-md-4 col-sm-4 col-xs-12"></div>
-        <div className="col-md-4 col-sm-4 col-xs-12">
-        
-          
-        <form className="form-container">
-          <img src="logo.png" alt=""></img>
-
-        
-        <div className="mb-3">
-        <input type="text" placeholder="Email" ref={emailRef} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-        </div>
-        <div className="mb-3">
-        <input type="password" placeholder="Password" ref={passwordRef} className="form-control" id="exampleInputPassword1"/>
-        </div>
-        <div className="mb-3 form-check"></div>
-        <div className="d-grid"> 
-        <button type="submit" className="btn btn-success btn-primary" disabled={loading || currentUser } onClick={handleLogin}>Log In</button>
-        </div>
-        </form>
-
-          
-
-
-          
-          
-        </div>
-        <div className="col-md-4 col-sm-4 col-xs-12"></div>
-
-          
-
-
-        </div>
-      </div>
-      </div>
-    </div>
-    
-  );
-}
+export default App;
 
  
 
 
  
 
-  async function handleLogin() {
-    setloading(true)
-
-
-    try{
-      await login(emailRef.current.value, passwordRef.current.value)
-    } catch{alert("Error !")}
-
-    setloading(false);
-
-  }
 
   
 
 
-  
-}
+
 
 
---
+++ src/Register.js
@@ -0,0 +1,9 @@
+export default function Register(){
+    return(
+        <div>
+            <h1> hej </h1>
+        </div>
+    );
+
+}
+
